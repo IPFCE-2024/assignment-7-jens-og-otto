cea989d5cca28b8db897bae225a2a6422005b362
NorthCreek <ottobilenberg@gmail.com>
2024-10-10 13:51:50 +0200

    push push

--- taylor_sine.c
+++ taylor_sine.c
@@ -2,5 +2,19 @@
 
 double taylor_sine(double x, int n) {
   // implement your function here
-  return -1.0;
+  
+  int sign = 1;
+  double taylor_sum = 0, term = x, fact_val = 1;
+  
+  for (int i = 0; i < n; i++)
+  {
+    taylor_sum += sign * term / fact_val;
+
+    sign = -sign;
+
+    term *= x * x;
+
+    fact_val *= (2 * i + 2) * ( 2 * i + 3);
+  }
+  return taylor_sum;
 }
